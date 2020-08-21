63ef30cb6821a34f2e1549c2dec37e430bb4215f
daisy0905 <dtong0905@gmail.com>
2020-08-20 20:46:53 -0600

    Created simple user array

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
-var age = 18;
-var is_subscribed = true;
+var age = [14, 17, 18, 20, 24, 30];
+var is_subscribed= [false, true, true, false, false, true];
+var arrayLength = age.length;
 
-if(age < 18 && !is_subscribed) {
-    console.log("The user is younger than 18 and is not subscribed");
-} else if(age >= 18 && !is_subscribed) {
-    console.log("The user is 18 and older and is not subscribed");
-} else if(age < 18 && is_subscribed) {
-    console.log("The user is younger than 18 and is subscribed");
-} else if(age >= 18 && is_subscribed) {
-    console.log("The user is 18 and older and is subscribed");
-} else {
-    console.log("Error!");
+for(var counter = 0; counter < age.length; counter++) {
+    if(age[counter] < 18 && !is_subscribed[counter]) {
+        console.log("No." + [counter+1])
+        console.log("age:" + age[counter]);
+        console.log("subscription status:" + is_subscribed[counter]);
+        console.log("The user is younger than 18 and is not subscribed");
+    } else if(age[counter] >= 18 && !is_subscribed[counter]) {
+        console.log("No." + [counter+1])
+        console.log("age:" + age[counter]);
+        console.log("subscription status:" + is_subscribed[counter]);
+        console.log("The user is 18 and older and is not subscribed");
+    } else if(age[counter] < 18 && is_subscribed[counter]) {
+        console.log("No." + [counter+1])
+        console.log("age:" + age[counter]);
+        console.log("subscription status:" + is_subscribed[counter]);
+        console.log("The user is younger than 18 and is subscribed");
+    } else if(age[counter] >= 18 && is_subscribed[counter]) {
+        console.log("No." + [counter+1])
+        console.log("age:" + age[counter]);
+        console.log("subscription status:" + is_subscribed[counter]);
+        console.log("The user is 18 and older and is subscribed");
+    } else {
+        console.log("Error!");
+    }
 }
